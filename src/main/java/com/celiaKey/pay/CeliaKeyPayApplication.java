6d81c529f4b80b15a3ca954d535253848d196390
11701 <1170138823>
2022-03-28 22:37:51 +0800

    支付系统框架初始化

---
+++ src/main/java/com/celiaKey/pay/CeliaKeyPayApplication.java
@@ -0,0 +1,13 @@
+package com.celiaKey.pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeliaKeyPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeliaKeyPayApplication.class, args);
+	}
+
+}
